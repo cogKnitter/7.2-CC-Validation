7970b73a1bdaa8976a66481915aa6c62fd4e4ec1
Mariah Tree <mariah.tree@gmail.com>
2016-05-24 16:46:07 -0500

    inputs added

--- modules/App.js
+++ modules/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import Validator from 'validator'
 
 export default React.createClass({
+  onHandleCardNumChange(e){
+    // if card number not entered correctly add error class
+  },
+  onHandleExpDateChange(e){
+    // if date before today add error class
+  },
+  onHandleCvvChange(e){
+    // if CVV not complete (3 nums) add error class
+  },
+  onHandleCardNameChange(e){
+    // if name less than 3 chars add error class
+  },
   render() {
     return (
-      <h1 className="heading">Hello!</h1>
+      <form>
+        <div>
+          <input className="card__num" type="number" placeholder="XXXXXXXXXXXXXXXX" onChange={this.onHandleCardNumChange}/>
+          <input className="exp__date" type="date" placeholder="YYYY-MM-DD" min="2016-05-31" onChange={this.onHandleExpDateChange}/>
+          <input className="cvv__num" type="number" placeholder="CVV" max="999" onChange={this.onHandleCvvChange}/>
+          <input className="card__name" type="text" placeholder="Full Name" onChange={this.onHandleCardNameChange}/>
+        </div>
+      </form>
     )
   }
 })
